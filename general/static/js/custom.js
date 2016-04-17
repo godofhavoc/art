58d6bb1630a548a8cd8351518214e64d7cb25c5e
memnonila <akshay163@gmail.com>
2016-04-17 23:42:45 +0530

    third commit

--- general/static/js/custom.js
+++ general/static/js/custom.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         $('.upcoming_exhibition').show();
         $('.previous_exhibition').hide();
         $('.current_exhibition').hide();
+        $('.street_art_festival').hide();
     });
 
     $('#previous').click(function() {
@@ -11,6 +12,7 @@ $(document).ready(function() {
         $('.upcoming_exhibition').hide();
         $('.previous_exhibition').show();
         $('.current_exhibition').hide();
+        $('.street_art_festival').hide();
     });
 
     $('#current').click(function() {
@@ -18,5 +20,14 @@ $(document).ready(function() {
         $('.upcoming_exhibition').hide();
         $('.previous_exhibition').hide();
         $('.current_exhibition').show();
+        $('.street_art_festival').hide();
+    });
+
+    $('#street_art').click(function() {
+        console.log('street art');
+        $('.upcoming_exhibition').hide();
+        $('.previous_exhibition').hide();
+        $('.current_exhibition').hide();
+        $('.street_art_festival').show();
     });
 });
